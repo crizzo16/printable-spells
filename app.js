f683f0499d9e1e98c0fee3aac95e3e2b0aeee15b
Celia Rizzo <crizzo16@users.noreply.github.com>
2021-07-09 15:30:06 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 let focus = {
     allChars: [],
-    num = 0,
+    num: 0,
     parseSpells: function () {
         $.ajax({
             url: "https://crizzo16.github.io/printable-spells/spells.json",
             dataType: "json"
         }).done(function (result) {
             focus.allChars = result;
+            focus.loadSpells();
         });
     },
-    loadSpells: function(num) {
+    loadSpells: function() {
+        $(".printable").html("");
         focus.allChars[focus.num].spells.forEach(function (item, index, array) {
-            console.log(item.name);
             let spell = $("<div>").addClass("spell-wrapper");
 
+            const borderStuff1 = $("<img>").addClass("spell-border bottom-right").attr("src", "./Upper Left.png");
+            const borderStuff2 = $("<img>").addClass("spell-border top-left").attr("src", "./Upper Left.png");
+            const borderStuff3 = $("<div>").addClass("triangle top-left");
+            const borderStuff4 = $("<div>").addClass("triangle bottom-right");
+            const borderStuff5 = $("<div>").addClass("box bottom-left");
+            const borderStuff6 = $("<div>").addClass("box top-right");
+
+            // Append all sections to spell
+            spell.append(spellContent).append(borderStuff1).append(borderStuff2).append(borderStuff3).append(borderStuff4).append(borderStuff5).append(borderStuff6);
             $(".printable").append(spell);
         });
+    },
+    formatLevel: function(num) {
+        if (num == 0) {
+            return "Cantrip";
+        } else if (num == 1) {
+            return "1st Level";
+        } else if (num == 2) {
+            return "2nd Level";
+        } else if (num == 3) {
+            return "3rd Level";
+        } else {
+            return num + "th Level";
+        }
     }
 };
+
+$(document).ready(function() {
+    focus.parseSpells();
+});
